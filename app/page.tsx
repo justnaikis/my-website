d6051f88bc2ab7fb73603dba3d038f32a2225e4d
Justnaikis <geceviciusvejas@gmail.com>
2026-01-09 22:33:33 +0200

    100 to 50

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 
-  const repeats = 100;
+  const repeats = 50;
   const itemsLookAtMe = Array.from({ length: repeats }).map((_, i) => (
     <div key={i} className="text-2xl font-bold marquee-item">Look at Me!</div>
   ));
@@ -14,14 +14,14 @@ export default function Home() {
       <div className="flex justify-center items-center">
 
         <div className="group relative h-25 w-full border-t-2 overflow-hidden">
-          <div className="absolute inset-0 bg-linear-to-r from-blue-500 via-purple-500 to-pink-500 flex items-center justify-center p-4">
+          <div className="absolute inset-0 bg-linear-to-r from-blue-500 via-purple-500 to-pink-500 flex items-center justify-center">
             <div className="marquee w-full">
               <div className="marquee-track">
                 {itemsLookAtMe}
               </div>
             </div>
           </div>
-          <div className="absolute inset-0 bg-black flex items-center justify-center p-4 transition-transform duration-300 group-hover:-translate-y-full">
+          <div className="absolute inset-0 bg-black flex items-center justify-center transition-transform duration-300 group-hover:-translate-y-full">
             <div className="text-2xl font-bold">
               Look at Me!
             </div>
@@ -31,14 +31,14 @@ export default function Home() {
       <div className="flex justify-center items-center">
 
         <div className="group relative h-25 w-full border-t-2 overflow-hidden">
-          <div className="absolute inset-0 bg-linear-to-r from-blue-500 via-purple-500 to-pink-500 flex items-center justify-center p-4">
+          <div className="absolute inset-0 bg-linear-to-r from-blue-500 via-purple-500 to-pink-500 flex items-center justify-center">
             <div className="marquee w-full">
               <div className="marquee-track">
                 {itemsAboutMe}
               </div>
             </div>
           </div>
-          <div className="absolute inset-0 bg-black flex items-center justify-center p-4 transition-transform duration-300 group-hover:-translate-y-full">
+          <div className="absolute inset-0 bg-black flex items-center justify-center transition-transform duration-300 group-hover:-translate-y-full">
             <div className="text-2xl font-bold">
               About Me!
             </div>
